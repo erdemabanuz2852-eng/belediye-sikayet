59d2be94b08398b6d80012be527b8af204a0c844
erdemabanuz2852-eng <erdemabanuz2852@gmail.com>
2025-08-28 01:00:50 +0300

    Delete belediye_sikayet_ready directory

--- belediye_sikayet_ready/sikayetler/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.SikayetListView.as_view(), name='sikayet_list'),
-    path('yeni/', views.SikayetCreateView.as_view(), name='sikayet_create'),
-    path('<int:pk>/', views.sikayet_detay, name='sikayet_detail'),
-]
